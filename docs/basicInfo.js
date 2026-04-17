446f72b2be4dd10155a13311c9d933da194513cd
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1241955.cfp127aluno.local>
2026-04-17 09:58:42 -0300

    docs(Swagger): Desenvolvendo o arquivo basicInfo.js, adicionando as informaçÕes básicas para a documentação

---
+++ docs/basicInfo.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "openapi": "3.0.0",
+
+    "info": {
+        "title": "Espectra - Auxiliando quem cuida",
+        "description": "Documentação da API da aplicação Espectra",
+
+        "x-contributors": [
+            {
+                "name": "Nicolas dos Santos",
+                "github": "https://github.com/nicolas16-sd"
+            },
+            {
+                "name": "Enzo Felix",
+                "github": "https://github.com/EnzoCarrilho"
+            },
+            {
+                "name": "Gabriel Lacerda",
+                "github": "https://github.com/GabrielPKTN"
+            },
+            {
+                "name": "Maria Cecília",
+                "github": "https://github.com/mariacecilia-01"
+            },
+            {
+                "name": "Aline Alves",
+                "github": "https://github.com/AlineMaker"
+            }
+        ],
+
+        "version": "1.0.0",
+
+        "tags": [
+            ''
+        ]
+    }
+
+}
