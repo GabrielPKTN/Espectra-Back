3f85c3fa1d513256b52333c0f56ab438edb65cc7
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1241955.cfp127aluno.local>
2026-04-17 11:13:01 -0300

    doc(Swagger): Adicionando a documentação swagger em relação ao responsável

---
+++ docs/internal/responsavel/put.js
@@ -0,0 +1,83 @@
+module.exports = {
+    post: {
+        tags: ['EndPoints [RESPONSÁVEL]'],
+        description: "Atualiza um responsável no sistema",
+        operationId: "atualizarResponsavel",
+        requestBody: {
+            content: {
+                "application/json": {
+                    schema: {
+                        $ref: "#/components/schema/responsableUpdate"
+                    }
+                }
+            }
+        },
+
+        responses: {
+            200: {
+                description: "Requisição bem sucedida",
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schema/responsableGet"
+                        }
+                    }
+                }
+            },
+            400: {
+                description: "Campo inválido",
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schema/error400"
+                        }
+                    }
+                }
+            },
+            404: {
+                description: "O Id informado não foi encontrado",
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schema/error404"
+                        }
+                    }
+                }
+            },
+            415: {
+                description: "Tipos de dados inválidos",
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schema/error415"
+                        }
+                    }
+                }
+            },
+            500: {
+                description: "Não foi possível processar a requisição por erros internos da Controller",
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schema/error500_controller"
+                        }
+                    }
+                }
+            },
+            500: {
+                description: "Não foi possível processar a requisição por erros internos da Model",
+                content: {
+                    "appkication/json": {
+                        schema: {
+                            "application/json": {
+                                schema: {
+                                    $ref: "#/components/schema/error500_model"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
