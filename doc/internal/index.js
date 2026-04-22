da8d44cc11215dfcc6d2012de39c89b8b53bfcc2
GABRIEL LACERDA CORREIA <25132902@dsn127m1246727.cfp127aluno.local>
2026-04-22 10:45:54 -0300

    doc: Adiciona import de components das entidades da aplicação

--- doc/internal/index.js
+++ doc/internal/index.js
@@ -1,9 +1,7 @@
 const reponsavel      = require('./responsavel')
-const paciente        = require('./paciente')
 const psicopedagogo   = require('./psicopedagogo')
 
 module.exports = {
     ...psicopedagogo,
-    ...responsavel,
-    ...paciente
+    ...responsavel
 }
