a594e56edcc3c8098f6279e04763c154c8f69b48
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-04-23 22:59:43 -0300

    feat: Adiciona endpoint de inserção de relação entre paciente e responsável

--- doc/internal/paciente/index.js
+++ doc/internal/paciente/index.js
@@ -4,6 +4,7 @@ const getByRegisterNumber       = require('./getByRegisterNumber.js')
 const post                      = require('./post.js')
 const put                       = require('./put.js')
 const postPacientePsicopedagogo = require('./postPacientePsicopedagogo.js')
+const postPacienteResponsavel   = require('./postPacienteResponsavel.js')
 
 module.exports = {
     
@@ -23,6 +24,10 @@ module.exports = {
 
     "/v1/espectra/paciente/?id_paciente={id}&id_psicopedagogo{id}": {
         ...postPacientePsicopedagogo
+    },
+
+    "/v1/espectra/paciente/?id_paciente={id}&id_paciente={id}": {
+        ...postPacienteResponsavel
     }
 
 }
