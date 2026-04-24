976f68bf747a9b85ca9dc3dd6534e533c9bf6ab9
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-04-23 22:36:14 -0300

    fix: Conserta rota GET que retorna o paciente pelo número de registro

--- doc/components/paciente.js
+++ doc/components/paciente.js
@@ -106,6 +106,57 @@ module.exports = {
         }
     },
 
+    pacienteGet: {
+        type: 'object',
+        properties: {
+            "id": {
+                "type": "int",
+                "description": "id",
+                "example": 1
+            },
+            "foto": {
+                "type": "string",
+                "description": "photo",
+                "example": "http://azure.blob.img"
+            },
+            "nome": {
+                "type": "string",
+                "description": "name",
+                "example": "Mario Augusto Ramos"
+            },
+            "data_nascimento": {
+                "type": "string",
+                "description": "birth_date",
+                "example": "2010-08-12"
+            },
+            "idade": {
+                "type": "int",
+                "description": "years",
+                "example": "12"
+            },
+             "diagnostico": {
+                "type": "string",
+                "description": "diagnostico",
+                "example": "Autismo e TDAH"
+            },
+            "serie_escolar": {
+                "type": "string",
+                "description": "série escolar",
+                "example": "2º Série"
+            },
+            "grau_suporte": {
+                "type": "string",
+                "description": "grau de suporte",
+                "example": "Grau 3"
+            },
+            "numero_registro": {
+                "type": "string",
+                "description": "numero de resgistro",
+                "example": "2026040001"
+            }
+        }
+    },
+
     //componente para atualizar o paciente
     pacientePut: {
         type: 'object',
