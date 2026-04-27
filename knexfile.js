7644f4a3b0bb6d0c79c27597378a04723cd4a4c8
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1241955.cfp127aluno.local>
2026-04-27 08:42:54 -0300

    feat: Adicionando documentação para arquivos de db e knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,11 @@
+/**********************************************************************
+ * Objetivo: Arquivo responsável pela configuração da ferramenta Knex
+ * Espectra
+ * Data: 27/04/2026
+ * Developer: Nicolas dos Santos
+ * Versão: 1.0.0
+ *********************************************************************/
+
 module.exports = {
   development: {
     client: 'mysql2',
--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,11 @@
+/**********************************************************************
+ * Objetivo: Arquivo responsável pela conexão do Knex com o Banco de Dados
+ * Espectra
+ * Data: 27/04/2026
+ * Developer: Nicolas dos Santos
+ * Versão: 1.0.0
+ *********************************************************************/
+
 const knex = require('knex')
 const config = require('../../knexfile')
 
