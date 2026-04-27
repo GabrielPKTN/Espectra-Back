d069939f29c05c77a108dee97a721bef01147399
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1241955.cfp127aluno.local>
2026-04-27 08:31:59 -0300

    feat: Instalando a ferramenta Knex e estabelecendo conexão com o banco da aplicação

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '12345678',
+      database: 'db_espectra'
+    }
+  }
+};
