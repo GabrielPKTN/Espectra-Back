55182d331ad8c5cf16d610f1e352ba778bab420b
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1241955.cfp127aluno.local>
2026-04-29 10:35:47 -0300

    feat: Inicia desenvolvimento do CRUD de Psicopedagogo

---
+++ src/controller/psicopedagogo/controllerPsicopedagogo.js
@@ -0,0 +1,9 @@
+/********************************************************************************
+ * Objetivo: Arquivo responsável pela manipulação de dados entre App e a Model
+ * Data: 29/04/2026
+ * Autores: Nicolas dos Santos e Enzo Carrilho
+ * Versão: 1.0
+ ********************************************************************************/
+
+const psicopedagogoDAO = require("../../model/DAO/psicopedagogo.js")
+
---
+++ src/model/DAO/psicopedagogo.js
@@ -0,0 +1,9 @@
+/*****************************************************************************
+ * Objetivo: Arquivo responsável pela integração pelo CRUD de dados do MySQL
+ * Data: 29/04/2026
+ * Autores: Nicolas dos Santos e Enzo Carrilho
+ * Versão: 1.0
+ ******************************************************************************/
+
+const db = require("../../database/db")
+
