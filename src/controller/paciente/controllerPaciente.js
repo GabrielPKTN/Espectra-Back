8541fe7e79cc806f2c69b7927714d851aef5466d
ENZO FELIX CARRILHO <25132408@dsn127m1246729.cfp127aluno.local>
2026-04-30 11:25:14 -0300

    feat: Inicio da controller de paciente

---
+++ src/controller/paciente/controllerPaciente.js
@@ -0,0 +1,13 @@
+/***********************************************************************************************
+ * Objetivo: Arquivo responsável pela manipulação de dados entre App e a Model do Paciente
+ * Data: 30/04/2026
+ * Autores: Enzo Carrilho
+ * Versão: 1.0
+ **********************************************************************************************/
+
+const patientDAO = require('../../model/DAO/paciente.js')
+const defaultMessages = require('../modulo/defaultMessages.js')
+
+const getPatient = async function(id) {
+    
+}
