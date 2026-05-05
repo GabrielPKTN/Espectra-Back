3199d6d5ffc7fd3b0251b37a6c1ad1bdb9c44d9d
ENZO FELIX CARRILHO <25132408@dsn127m1246729.cfp127aluno.local>
2026-05-05 11:25:00 -0300

    feat: validacao de paciente na controller

--- src/controller/paciente/controllerPaciente.js
+++ src/controller/paciente/controllerPaciente.js
@@ -5,7 +5,6 @@
  * Versão: 1.0
  **********************************************************************************************/
 
-const e = require('express')
 const patientDAO = require('../../model/DAO/paciente.js')
 const defaultMessages = require('../modulo/defaultMessages.js')
 
@@ -66,23 +65,59 @@ const getPatientByRegistNumber = async function(registNumber){
     }
 }
 
-const setPatient = async function(patient) {
+const setPatient = async function(patient, contentType) {
     let MESSAGES = JSON.parse(JSON.stringify(defaultMessages))
 
     try {
-        
+
+        if(String(contentType).toUpperCase() == 'APPLICATION/JSON'){
+            let validate = await validatePatient(patient)
+            
+            if(!validate){
+                let 
+            }
+        }
+       
+
     } catch (error) {
          return MESSAGES.errorInternalServer //500
     }
 }
 
+
+
 const validatePatient = function(patient){
-    if(patient.nome == '' || patient.nome == null || patient.nome == undefined){
+    let MESSAGES = JSON.parse(JSON.stringify(defaultMessages))
+
+    if(patient.nome == '' || patient.nome == null || patient.nome == undefined || patient.nome.length > 150){
         MESSAGES.errorRequiredFields.message += ' [Nome Inválido]' 
         return MESSAGES.errorRequiredFields //400
 
-    }else if(patient.data_nascimento == '' || patient.data_nascimento == null || patient.data_nascimento == undefined){
+    }else if(patient.data_nascimento == '' || patient.data_nascimento == null || patient.data_nascimento == undefined || patient.data_nascimento.length != 10){
+        MESSAGES.errorRequiredFields.message += ' [Data de nascimento Inválido]' 
+        return MESSAGES.errorRequiredFields //400
 
+    }else if(patient.diagnostico.length > 50){
+        MESSAGES.errorRequiredFields.message += ' [Diagnóstico Inválido]' 
+        return MESSAGES.errorRequiredFields //400
+
+    }else if(patient.foto.length > 255){
+        MESSAGES.errorRequiredFields.message += ' [Foto Inválido]' 
+        return MESSAGES.errorRequiredFields //400
+
+    }else if(patient.id_serie_escolar <= 0 || isNaN(patient.id_serie_escolar) || patient.id_serie_escolar == '' || patient.id_serie_escolar == null || patient.id_serie_escolar == undefined){
+        MESSAGES.errorRequiredFields.message += ' [id_serie_escolar Inválido]' 
+        return MESSAGES.errorRequiredFields //400
+
+    }else if(patient.id_grau_suporte <= 0 || isNaN(patient.id_grau_suporte) || patient.id_grau_suporte == '' || patient.id_grau_suporte == null || patient.id_grau_suporte == undefined){
+        MESSAGES.errorRequiredFields.message += ' [id_grau_suporte Inválido]' 
+        return MESSAGES.errorRequiredFields //400
+
+    }else if(patient.id_responsavel <= 0 || isNaN(patient.id_responsavel) || patient.id_responsavel == '' || patient.id_responsavel== null || patient.id_responsavel == undefined){
+        MESSAGES.errorRequiredFields.message += ' [id_responsavel Inválido]' 
+        return MESSAGES.errorRequiredFields //400
+    }else{
+        return false
     }
 
 } 
