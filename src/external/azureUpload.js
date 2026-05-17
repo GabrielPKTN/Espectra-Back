58b927d322e4571476dccf49c31677bd2d71331f
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-05-17 13:31:42 -0300

    feat: Adiciona arquivos de configuração e serviço para upload de imagens na aplicação

---
+++ src/external/azureUpload.js
@@ -0,0 +1,38 @@
+/********************************************************************************
+ * Objetivo: Arquivo responsável pelo upload de fotos para a Azure
+ * Data: 17/05/2026
+ * Autores: Gabriel Lacerda
+ * Versão: 1.0
+ ********************************************************************************/
+
+const configAzure = require('../config/azure.js')
+const uuid = require('uuid')
+const path = require('path')
+
+// Responsável por enviar as fotos
+const uploadAzure = async (foto) => {
+
+    // Gera um UUID único + extensão original da foto
+    const extensao  = path.extname(foto.originalName)
+    const nomeUnico = `${uuid.uuidv4()}${extensao}`
+
+    // Client do blob com o nome gerado
+    const clientBlob = configAzure.clientContainer.getBlockBlobClient(nomeUnico);
+
+    // Realizando upload da foto
+    await clientBlob.upload(
+        foto.buffer,
+        foto.size, 
+        {
+        blobHTTPHeaders: {
+            blobContentType: foto.mimetype
+        }
+    })
+
+    return clientBlob.url
+
+}
+
+module.exports = {
+    uploadAzure
+}
