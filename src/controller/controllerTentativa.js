3b7c4bf01a30308afa8cec9e1fc0d8f5a5847534
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-05-20 19:59:52 -0300

    fix: Adiciona erro 500 vindo do banco

--- src/controller/controllerTentativa.js
+++ src/controller/controllerTentativa.js
@@ -17,30 +17,31 @@ const getTentativasByIdAtividade = async (id_atividade) => {
     MESSAGES = JSON.parse(JSON.stringify(defaultMessages))
 
     try {
-        
+
         id_atividade = Number(id_atividade)
 
         if (Number.isInteger(id_atividade) && id_atividade > 0) {
 
             result = await tentativaDAO.getTentativasByIdAtividade(id_atividade)
 
-            if(result) {
+            if (result) {
 
-                if(result.status_code == 200) {
+                if (result.status_code == 200) {
 
-                    MESSAGES.DEFAULT_HEADER.status      = MESSAGES.SUCCESS_REQUEST.status
+                    MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
                     MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
-                    MESSAGES.DEFAULT_HEADER.message     = MESSAGES.SUCCESS_REQUEST.message
-                    MESSAGES.DEFAULT_HEADER.items       = result.data
+                    MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_REQUEST.message
+                    MESSAGES.DEFAULT_HEADER.items = result.data
 
                     return MESSAGES.DEFAULT_HEADER //200
 
-                } else if(result.status_code == 404) {
+                } else if (result.status_code == 404) {
 
                     return MESSAGES.ERROR_NOT_FOUND //404
 
                 } else {
 
+                    MESSAGES.ERROR_INTERNAL_SERVER_MODEL.message + " [PROCEDURE]"
                     return MESSAGES.ERROR_INTERNAL_SERVER_MODEL //500
 
                 }
@@ -65,30 +66,35 @@ const postTentativa = async (tentativa, contentType) => {
     MESSAGES = JSON.parse(JSON.stringify(defaultMessages))
 
     try {
-        
+
         if (String(contentType).toUpperCase() == 'APPLICATION/JSON') {
 
             validar = validateAttempt(tentativa)
 
-            if(!validar) {
+            if (!validar) {
 
                 result = await tentativaDAO.postTentativa(tentativa)
 
-                if(result) {
-                    
-                    if(result.status_code == 200) {
+                if (result) {
 
-                        MESSAGES.DEFAULT_HEADER.status      = MESSAGES.SUCCESS_CREATED_ITEM.status
+                    if (result.status_code == 200) {
+
+                        MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_CREATED_ITEM.status
                         MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_CREATED_ITEM.status_code
-                        MESSAGES.DEFAULT_HEADER.message     = MESSAGES.SUCCESS_CREATED_ITEM.message
-                        MESSAGES.DEFAULT_HEADER.items       = result.data
+                        MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_CREATED_ITEM.message
+                        MESSAGES.DEFAULT_HEADER.items = result.data
 
                         return MESSAGES.DEFAULT_HEADER //201
 
-                    } else if(result.status_code == 404) {
+                    } else if (result.status_code == 404) {
+
                         return MESSAGES.ERROR_NOT_FOUND //404
+
                     } else {
+
+                        MESSAGES.ERROR_INTERNAL_SERVER_MODEL.message + " [PROCEDURE]"
                         return MESSAGES.ERROR_INTERNAL_SERVER_MODEL //500
+
                     }
 
                 } else {
@@ -109,40 +115,40 @@ const postTentativa = async (tentativa, contentType) => {
 
 }
 
-const validateAttempt = function(attempt){
+const validateAttempt = function (attempt) {
 
     let MESSAGES = JSON.parse(JSON.stringify(defaultMessages))
 
-    if(attempt.resultado == '' || attempt.resultado == null || attempt.resultado == undefined || attempt.resultado > 1 || attempt.resultado < 0 ){
-        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [resultado Inválido]' 
+    if (attempt.resultado == '' || attempt.resultado == null || attempt.resultado == undefined || attempt.resultado > 1 || attempt.resultado < 0) {
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [resultado Inválido]'
         return MESSAGES.ERROR_REQUIRED_FIELDS //400
 
-    }else if(attempt.data_tentativa == '' || attempt.data_tentativa == null || attempt.data_tentativa == undefined || attempt.data_tentativa.length != 10){
-        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [Data Inválido]' 
+    } else if (attempt.data_tentativa == '' || attempt.data_tentativa == null || attempt.data_tentativa == undefined || attempt.data_tentativa.length != 10) {
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [Data Inválido]'
         return MESSAGES.ERROR_REQUIRED_FIELDS //400
 
-    }else if(attempt.observacao != null) {
+    } else if (attempt.observacao != null) {
 
-        if(!isNaN(attempt.observacao) && attempt.observacao.length > 1500) {
+        if (!isNaN(attempt.observacao) && attempt.observacao.length > 1500) {
 
-            MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [OBSERVAÇÃO INVÁLIDA]' 
+            MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [OBSERVAÇÃO INVÁLIDA]'
             return MESSAGES.ERROR_REQUIRED_FIELDS //400
 
         }
-    
-    }else if(attempt.id_auxilio <= 0 || isNaN(attempt.id_auxilio) || attempt.id_auxilio == '' || attempt.id_auxilio == null || attempt.id_auxilio == undefined){
-        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [id_tipo_aplicacao Inválido]' 
+
+    } else if (attempt.id_auxilio <= 0 || isNaN(attempt.id_auxilio) || attempt.id_auxilio == '' || attempt.id_auxilio == null || attempt.id_auxilio == undefined) {
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [id_tipo_aplicacao Inválido]'
         return MESSAGES.ERROR_REQUIRED_FIELDS //400
 
-    }else if(attempt.id_atividade <= 0 || isNaN(attempt.id_atividade) || attempt.id_atividade == '' || attempt.id_atividade == null || attempt.id_atividade == undefined){
-        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [id_atividade Inválido]' 
+    } else if (attempt.id_atividade <= 0 || isNaN(attempt.id_atividade) || attempt.id_atividade == '' || attempt.id_atividade == null || attempt.id_atividade == undefined) {
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [id_atividade Inválido]'
         return MESSAGES.ERROR_REQUIRED_FIELDS //400
 
-    }else{
+    } else {
         return false
     }
 
-} 
+}
 
 module.exports = {
     getTentativasByIdAtividade,
